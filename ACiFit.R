3d19b50bd0eba173a1cd61c24124051bf21f317f
Liat Adler <liatadler@hotmail.com>
2021-09-19 23:13:05 +0100

    Tidied code up

--- ACiFit.R
+++ ACiFit.R
@@ -1,25 +1,25 @@
+#import libraries
 library(plantecophys);
 library(readr);
 
-#import raw Licor csv file, skip the first 11 rows so that the 'photo' and 'Ci' etc. become headers
-#col_names = TRUE,
-mydata <- read_csv("Desktop/AciTest2.csv", skip = 11, col_types = list(
-  Photo = col_double(),
-  Tleaf = col_double(),
-  PARi = col_double(),
-  Rd = col_double(),
-  Ci = col_double()));
+#import raw Licor csv file, skip the first 11 rows so that the headers are in 1st row
+raw_licor_data <- read_csv("Desktop/AciTest2.csv", skip = 11, col_types = list(
+  Photo = col_double(), #make sure column is numeric
+  Tleaf = col_double(), #make sure column is numeric
+  PARi = col_double(),  #make sure column is numeric
+  Rd = col_double(),    #make sure column is numeric
+  Ci = col_double()));  #make sure column is numeric
 
-#extract only the relevant rows so we have only the column headings 
-#and the headings for each measurement
-ACiraw2 = mydata[c(13,19,25,37,43,49,55,61,67,73,79,85,91), ];
+#convert raw data into usable format by extracting only the relevant rows so we have only the column headings 
+#and the values for each measurement
+converted_licor_data = raw_licor_data[c(13,19,25,37,43,49,55,61,67,73,79,85,91), ];
 
-#run the plantecophys ACi fit function
-fit = fitaci(ACiraw2);
+#run the plantecophys ACi fit function on the converted licor data
+fit = fitaci(converted_licor_data);
 
 #view the parameters
 fit;
 
-#plot the Aci Curve
+#plot the ACi Curve
 plot(fit)
 
