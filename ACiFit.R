69f7c7c5a0ebc66f82c3f90986f47314e2adfea8
Liat Adler <liatadler@hotmail.com>
2021-09-19 17:10:06 +0100

    converts raw licor to usable format
    Now imports the raw licor csv file. Uses only rows with the necessary headers and the data from each measurement. Also converts the columns used by platecophs fitaci function to doubles.

--- ACiFit.R
+++ ACiFit.R
@@ -1,9 +1,25 @@
-#install.packages("plantecophys")
-library(plantecophys)
-mydata <- read.csv("Users/liatadler/Desktop/AciTest1.csv")
+library(plantecophys);
+library(readr);
 
-fit <- fitaci(AciTest1)
-fit
+#import raw Licor csv file, skip the first 11 rows so that the 'photo' and 'Ci' etc. become headers
+#col_names = TRUE,
+mydata <- read_csv("Desktop/AciTest2.csv", skip = 11, col_types = list(
+  Photo = col_double(),
+  Tleaf = col_double(),
+  PARi = col_double(),
+  Rd = col_double(),
+  Ci = col_double()));
+
+#extract only the relevant rows so we have only the column headings 
+#and the headings for each measurement
+ACiraw2 = mydata[c(13,19,25,37,43,49,55,61,67,73,79,85,91), ];
+
+#run the plantecophys ACi fit function
+fit = fitaci(ACiraw2);
+
+#view the parameters
+fit;
+
+#plot the Aci Curve
 plot(fit)
-
 
