cb55b4ed3b3686c82ce43ee7c92d29b04d4dc52f
Liat Adler <liatadler@hotmail.com>
2021-10-17 21:07:30 +0100

    Add files via upload

--- Using_lapply_inprogress.R
+++ Using_lapply_inprogress.R
@@ -1,12 +1,11 @@
 
 #give the source of the script that contains the function
 source("Desktop/ACi-convert-Fit-Function.R");
-#ACi_convert_Fit("Desktop/AciTest2.csv");
 
-#get a list of all .csv files in directory
-all_Aci_files <- list.files("TestFolder1", pattern = "*.csv",
+#get a list of all .csv files in directory of choice
+all_Aci_files <- list.files("Desktop/TestFolder1", pattern = "*.csv",
                                full.names = TRUE);
 
 # use lapply function to apply the function to every file in the list
-lapply(X = all_Aci_files,   # List or vector
-       FUN = ACi_convert_Fit()); # Function to be applied) # Additional arguments to be passed to FUN
+lapply(X = all_Aci_files,   # give the list of files to be batch processed
+       FUN = ACi_convert_Fit); # define the function to be applied 
